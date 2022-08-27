71345572dbcb5c8e3c114ecb036ea55bb454af50
Adithya <jhadith88@gmail.com>
2022-08-27 07:47:57 +0530

    bbb

--- collies/pages/api/auth/[...nextauth].js
+++ collies/pages/api/auth/[...nextauth].js
@@ -8,25 +8,25 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: "619457204981-anrsub5h11phahls2n9pskfd8kll3535.apps.googleusercontent.com",
+      clientSecret: "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",
     }),
     // ...add more providers here
 
     RedditProvider({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      clientId: "XD9pbLMNExAHfcv-UZoYTQ",
+      clientSecret: "welHs7HMKCqmTsRVUoiHw6WbyLE8_w",
     }),
 
     InstagramProvider({
-      clientId: process.env.INSTAGRAM_CLIENT_ID,
-      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET
+      clientId: "542646807356294",
+      clientSecret: "951f6bc3a6a93d49c5291aaec8369ada"
     }),
 
   
       LinkedInProvider({
-        clientId: process.env.LINKEDIN_CLIENT_ID,
-        clientSecret: process.env.LINKEDIN_CLIENT_SECRET
+        clientId: "86w01au60kd2c9",
+        clientSecret: "zmlxeSeTy5Fl6kX6"
       }),
   ],
   callbacks: {
